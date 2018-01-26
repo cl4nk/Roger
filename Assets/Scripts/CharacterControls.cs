007ba90708409569065c1e086e9b30923890890d
Million Quentin <q.million@student.isartdigital.com>
2018-01-26 22:44:12 +0100

    Add basic scripts for Controls

---
+++ Assets/Scripts/CharacterControls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControls : MonoBehaviour {
+
+    #region Fields
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    [SerializeField]
+    private Vector3 direction;
+
+    [SerializeField]
+    private Vector3 rotation;
+
+    #endregion
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        
+        
+
+	}
+
+    public void Move(Transform characterTransform)
+    {
+        float verticalTranslation = (Input.GetAxis("Vertical") * moveSpeed) * Time.deltaTime;
+        float horizontalTranslation = (Input.GetAxis("Horizontal") * moveSpeed) * Time.deltaTime;
+
+        characterTransform.Translate(horizontalTranslation, 0, 0);
+        characterTransform.Translate(0, verticalTranslation, 0);
+    }
+}
---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+    private CharacterControls test = null;
+
+
+    private void Awake()
+    {
+        test = GetComponent<CharacterControls>();
+    }
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+
+        test.Move(gameObject.transform);
+
+	}
+}
