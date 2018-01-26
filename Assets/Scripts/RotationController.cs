07c22538abaf3d74f7ffd7dce99d6ee7ffc6e0f2
Million Quentin <q.million@student.isartdigital.com>
2018-01-27 00:36:33 +0100

    Modify controls
    Create 2 scripts for Rotation and Translation

---
+++ Assets/Scripts/RotationController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour {
+
+    #region Fields
+
+    [SerializeField]
+    private Vector3 rotation = Vector3.zero;
+
+    [SerializeField]
+    private float rotationSpeed = 5f;
+
+    [SerializeField]
+    private string horizontalRotationAxis = "HorizontalRotation";
+
+    [SerializeField]
+    private string verticalRotationAxis = "VerticalRotation";
+
+    #endregion
+
+    void Start() {
+
+    }
+
+    void Update()
+    {
+        Rotate();
+    }
+
+    public void Rotate()
+    {
+        Vector3 forward = new Vector3(Input.GetAxis(horizontalRotationAxis), Input.GetAxis(verticalRotationAxis), 0.0f);
+
+        if (forward.sqrMagnitude > 0.0f)
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(forward, Vector3.forward), rotationSpeed * Time.deltaTime);
+    }
+}
