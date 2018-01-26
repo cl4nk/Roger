807ffe27b53ee24db15a01a2ba9e6ad274325290
n.fontes <n.fontes@student.isartdigital.com>
2018-01-26 22:23:41 +0100

    Add basic scripts for Character

---
+++ Assets/Scripts/Character/ArrivalZone.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+public class ArrivalZone : MonoBehaviour
+{
+    public static event Action<ArrivalZone> OnArriveEvent;
+
+    public bool OnlyOnce = false;
+
+    [SerializeField]
+    private Transform player;
+
+    public float AcceptanceRadius = 1.0f;
+
+    private bool AlreadyTriggered = false;
+
+    private bool PlayerIsInZone = false;
+
+    public bool CanBeTriggered
+    {
+        get
+        {
+            if (PlayerIsInZone)
+                return false;
+            return (OnlyOnce && !AlreadyTriggered) || !OnlyOnce;
+        }
+    }
+
+    public void OnEnable()
+    {
+        AlreadyTriggered = true;
+        OnArriveEvent += OnArrive;
+    }
+
+    public void OnDisable()
+    {
+        AlreadyTriggered = false;
+        OnArriveEvent -= OnArrive;
+    }
+
+    public void Update()
+    {
+        if (Vector3.Distance(player.position, transform.position) <= AcceptanceRadius)
+        {
+            if (CanBeTriggered && OnArriveEvent != null)
+                OnArriveEvent.Invoke(this);
+        }
+        else
+        {
+            PlayerIsInZone = false;
+        }
+    }
+
+    public void OnArrive(ArrivalZone zone)
+    {
+        if (zone == this)
+            AlreadyTriggered = true;
+    }
+
+}
---
+++ Assets/Scripts/Character/PlayerCharacter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour, IDamageable
+{
+    public event Action OnDeathEvent;
+    public event Action<float> OnDamageTakenEvent;
+
+    [SerializeField]
+    private float maxHP = 100.0f;
+    public float MaxHP
+    {
+        get { return maxHP; }
+    }
+
+    private float currentHP = 0.0f;
+    public float CurrentHP
+    {
+        get { return currentHP; }
+        private set
+        {
+            currentHP = value;
+            if (currentHP < 0.0f)
+            {
+                currentHP = 0.0f;
+                if (OnDeathEvent != null)
+                    OnDeathEvent();
+            }
+        }
+    }
+
+    protected void OnEnable()
+    {
+        CurrentHP = MaxHP;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        CurrentHP -= damage;
+        if (OnDamageTakenEvent != null)
+            OnDamageTakenEvent(CurrentHP);
+    }
+}
