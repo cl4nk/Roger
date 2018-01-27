47cb5cb2088197b6244e8e84a3ca421822a83b84
n.fontes <n.fontes@student.isartdigital.com>
2018-01-27 07:17:31 +0100

    Add noise detectors

---
+++ Assets/Scripts/AI/NoiseDetection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NoiseDetection : MonoBehaviour
+{
+    private static readonly List<NoiseDetection> noiseDetectors = new List<NoiseDetection>();
+    public static List<NoiseDetection> NoiseDetectors
+    {
+        get { return noiseDetectors; }
+    }
+
+    public static event Action<NoiseDetection, Vector3> StaticOnNoiseDetected;
+
+    public UnityEvent<Vector3> OnNoiseDetected;
+
+    [Range(0, float.MaxValue)]
+    public float Distance = 5.0f;
+
+    public void OnEnable()
+    {
+        NoiseDetectors.Add(this);
+    }
+
+    public void OnDisable()
+    {
+        NoiseDetectors.Remove(this);
+    }
+
+    public static void EmitNoise(Vector3 position, float range)
+    {
+        foreach (NoiseDetection detector in NoiseDetectors)
+        {
+            float noiseDistance = Vector3.Distance(position, detector.transform.position);
+            if (noiseDistance < range + detector.Distance)
+            {
+                if (detector.OnNoiseDetected != null)
+                    detector.OnNoiseDetected.Invoke(position);
+                if (StaticOnNoiseDetected != null)
+                    StaticOnNoiseDetected(detector, position);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Controls/WalkNoise.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class WalkNoise : MonoBehaviour
+{
+    public float DistanceBetween = 2.0f;
+
+    public float NoiseRange = 1.0f;
+
+    private Vector3 LastPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    LastPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float distance = Vector3.Distance(LastPosition, transform.position);
+	    if (distance > DistanceBetween)
+	    {
+	        NoiseDetection.EmitNoise(transform.position, NoiseRange);
+	        LastPosition = transform.position;
+	    }
+	}
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawSphere(LastPosition, 0.05f);
+    }
+}
