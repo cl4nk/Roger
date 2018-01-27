41140334969605d53b0c44aa2bd24bff0ea0d1ad
n.fontes <n.fontes@student.isartdigital.com>
2018-01-27 03:17:26 +0100

    Add Continous option

--- Assets/Scripts/RandomAudioSource.cs
+++ Assets/Scripts/RandomAudioSource.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 public class RandomAudioSource : MonoBehaviour
@@ -19,8 +20,58 @@ public class RandomAudioSource : MonoBehaviour
         }
     }
 
+    [SerializeField]
+    private bool continous = false;
+
+    public bool Continous
+    {
+        get { return continous; }
+        set
+        {
+            continous = value;
+            if (continous)
+            {
+                if (coroutine != null)
+                    coroutine = StartCoroutine(ContinousCoroutine());
+            }
+            else if (coroutine != null)
+            {
+                StopCoroutine(coroutine);
+                coroutine = null;
+            }
+        }
+    }
+
+    private Coroutine coroutine;
+
     public void OnEnable()
     {
         Source.clip = clips[Random.Range(0, clips.Length)];
+
+        if (Continous)
+        {
+            coroutine = StartCoroutine(ContinousCoroutine());
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+            coroutine = null;
+        }
+    }
+
+    private IEnumerator ContinousCoroutine()
+    {
+        while (true)
+        {
+            if (!Source.isPlaying)
+            {
+                Source.clip = clips[Random.Range(0, clips.Length)];
+            }
+            yield return null;
+        }
     }
 }
