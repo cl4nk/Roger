8f49e70587cc5f7e844346727072e40269cc5fb4
n.fontes <n.fontes@student.isartdigital.com>
2018-01-27 03:09:22 +0100

    Add RandomClipSrouce

---
+++ Assets/Scripts/RandomAudioSource.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class RandomAudioSource : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips;
+
+    private AudioSource source;
+    public AudioSource Source
+    {
+        get
+        {
+            if (source == null)
+            {
+                source = GetComponent<AudioSource>();
+            }
+            return source;
+        }
+    }
+
+    public void OnEnable()
+    {
+        Source.clip = clips[Random.Range(0, clips.Length)];
+    }
+}
