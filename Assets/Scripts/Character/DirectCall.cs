2d0fa4c7212eb4337708125526b383d6749d98b5
Julien Falcioni <dragon9000@hotmail.fr>
2018-01-28 12:33:34 +0100

    modify sound for triggers, add end

---
+++ Assets/Scripts/Character/DirectCall.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class DirectCall : MonoBehaviour {
+
+    public AudioClip clip;
+    public AudioClip clip2;
+    public AudioClip clip3;
+    public AudioClip endMusic;
+    public AudioSource source;
+    public AudioMixer mixer;
+
+    public AudioSource ambiant;
+
+    public Image fadeScreen;
+    public Image credits;
+
+    bool played = false;
+
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+	}
+	
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (played)
+            return;
+        played = true;
+        if (col.GetComponent<Player>() != null)
+        {
+            PlayEnd();
+        }
+    }
+
+    public void PlayEnd()
+    {
+        StartCoroutine(PlayEndEnum());
+    }
+
+    IEnumerator PlayEndEnum()
+    {
+        Player.Instance.GetComponent<TranslationController>().enabled = false;
+
+        source.clip = clip;
+
+        float t = 0.0f;
+        source.Play();
+        while(source.isPlaying)
+        {
+            t += Time.deltaTime;
+            fadeScreen.color = Color.Lerp(new Color(0.0f, 0.0f, 0.0f, 0.0f), new Color(0.0f, 0.0f, 0.0f, 1.0f), t / source.clip.length);
+            ambiant.outputAudioMixerGroup.audioMixer.SetFloat("Volume", Mathf.Lerp(-0.17f,-80.0f, t / source.clip.length));
+            yield return null;
+        }
+
+        fadeScreen.color = Color.black;
+
+        ambiant.outputAudioMixerGroup.audioMixer.SetFloat("Volume", -80.0f);
+
+        yield return new WaitForSeconds(2.0f);
+
+        source.clip = clip2;
+        source.Play();
+
+        while (source.isPlaying)
+        {
+            yield return null;
+        }
+
+        source.clip = clip3;
+        source.Play();
+
+        while (source.isPlaying)
+        {
+            yield return null;
+        }
+
+        source.clip = endMusic;
+        source.Play();
+
+        float fadeDuration = 2.0f;
+        t = 0.0f;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            credits.color = Color.Lerp(new Color(1.0f, 1.0f, 1.0f, 0.0f), new Color(1.0f, 1.0f, 1.0f, 1.0f), t / fadeDuration);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(30.0f);
+
+        t = 0.0f;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            credits.color = Color.Lerp(new Color(1.0f, 1.0f, 1.0f, 1.0f), new Color(1.0f, 1.0f, 1.0f, 0.0f), t / fadeDuration);
+            yield return null;
+        }
+
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/MissionFlow/CallSender.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class CallSender : MonoBehaviour {
+
+    public AudioClip[] clipsResponse;
+    public AudioClip[] clipsNoResponse;
+
+    AudioSource source;
+
+    bool played = false;
+
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (played)
+            return;
+        played = true;
+        
+        CallReceiver receiver = col.GetComponent<CallReceiver>();
+        if (receiver != null)
+        {
+            receiver.responseCallbacks.AddListener(() => PlaySoundsResponse() );
+            receiver.noResponseCallbacks.AddListener(() => PlaySoundsNoResponse());
+            receiver.StartRinging();
+        }
+    }
+
+    void PlaySoundsResponse()
+    {
+        StartCoroutine(PlaySoundsResponseEnum());
+    }
+
+    IEnumerator PlaySoundsResponseEnum()
+    {
+        for (int i = 0; i < clipsResponse.Length; i++)
+        {
+            source.clip = clipsResponse[i];
+            source.Play();
+
+            while (source.isPlaying)
+                yield return null;
+        }
+
+    }
+
+    void PlaySoundsNoResponse()
+    {
+        StartCoroutine(PlaySoundsNoResponseEnum());
+    }
+
+    IEnumerator PlaySoundsNoResponseEnum()
+    {
+        for (int i = 0; i < clipsNoResponse.Length; i++)
+        {
+            source.clip = clipsNoResponse[i];
+            source.Play();
+
+            while (source.isPlaying)
+                yield return null;
+        }
+    }
+
+
+
+}
