bbeb3e9a87134905ff1fcbc796d849f9c4597348
Julien Falcioni <dragon9000@hotmail.fr>
2018-01-28 02:45:48 +0100

    minigame aux petits oignons

--- Assets/Plugins/FMOD/RuntimeUtils.cs
+++ Assets/Plugins/FMOD/RuntimeUtils.cs
@@ -413,8 +413,7 @@ namespace FMODUnity
                     return FMODPlatform.Linux;
                 case BuildTarget.StandaloneOSXIntel:
                 case BuildTarget.StandaloneOSXIntel64:
-                case BuildTarget.StandaloneOSX:
-                    return FMODPlatform.Mac;
+
                 case BuildTarget.StandaloneWindows:
                 case BuildTarget.StandaloneWindows64:
                     return FMODPlatform.Windows;
